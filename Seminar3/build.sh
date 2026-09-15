e8e74e272453c122f03f766964b0e5cd69c8cf60
Stefan Nožinić <stefan@lugons.org>
2026-09-15 17:13:06 +0200

    Initial seminar 3 presentation

--- Seminar3/build.sh
+++ Seminar3/build.sh
@@ -13,4 +13,10 @@ pandoc $PAPER \
   --to pdf \
   --citeproc \
   --bibliography $REFS \
-  --output "${THIS_DIR}/paper.pdf" 
+  --output "${THIS_DIR}/paper.pdf"
+
+PRESENTATION_DIR="${THIS_DIR}/presentation"
+
+cd "${PRESENTATION_DIR}"
+xelatex -interaction=nonstopmode -halt-on-error presentation.tex
+xelatex -interaction=nonstopmode -halt-on-error presentation.tex 
